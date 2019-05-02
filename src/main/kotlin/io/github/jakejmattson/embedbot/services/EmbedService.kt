e99debc8294229fd4f27389c3f2bad2578e50d6a
Jake Mattson <JakeJMattson@gmail.com>
2019-05-01 22:30:00 -0500

    Create EmbedService.kt

---
+++ src/main/kotlin/io/github/jakejmattson/embedbot/services/EmbedService.kt
@@ -0,0 +1,30 @@
+package io.github.jakejmattson.embedbot.services
+
+import me.aberrantfox.kjdautils.api.annotation.Service
+import me.aberrantfox.kjdautils.api.dsl.embed
+import net.dv8tion.jda.core.EmbedBuilder
+import net.dv8tion.jda.core.entities.*
+
+data class Embed(val name: String, val builder: EmbedBuilder = EmbedBuilder())
+
+@Service
+class EmbedService {
+    private val embedMap = HashMap<String, ArrayList<Embed>>()
+
+    private fun getGuildEmbeds(guildId: String) : ArrayList<Embed> {
+        if (!embedMap.containsKey(guildId))
+            embedMap[guildId] = arrayListOf()
+
+        return embedMap[guildId]!!
+    }
+
+    fun addEmbed(guild: Guild, name: String) {
+        getGuildEmbeds(guild.id).add(Embed(name))
+    }
+
+    fun removeEmbed(guild: Guild, name: String) {
+        getGuildEmbeds(guild.id).remove(Embed(name))
+    }
+
+    fun listEmbeds(guild: Guild) = getGuildEmbeds(guild.id).joinToString("\n") { it.name }
+}
