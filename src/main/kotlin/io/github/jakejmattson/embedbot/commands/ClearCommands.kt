487935c006166b462590364908b1e7250d810405
Jake Mattson <JakeJMattson@gmail.com>
2019-06-04 16:51:18 -0500

    Add more templates
    Generate clear commands with a function instead of statically typing each one.
    Add String templates for command descriptions.

--- src/main/kotlin/io/github/jakejmattson/embedbot/commands/ClearCommands.kt
+++ src/main/kotlin/io/github/jakejmattson/embedbot/commands/ClearCommands.kt
@@ -1,88 +1,28 @@
 package io.github.jakejmattson.embedbot.commands
 
-import io.github.jakejmattson.embedbot.services.getLoadedEmbed
+import io.github.jakejmattson.embedbot.services.*
 import me.aberrantfox.kjdautils.api.dsl.*
-import kotlin.streams.toList
 
 @CommandSet("Clear")
 fun clearCommands() = commands {
-    command("ClearAuthor") {
-        requiresGuild = true
-        description = "Clear the author from the currently loaded embed."
-        execute {
-            val embed = getLoadedEmbed(it.guild!!)!!
-            embed.clearAuthor()
-            it.respond("Author cleared.")
-        }
-    }
 
-    command("ClearColor") {
-        requiresGuild = true
-        description = "Clear the color from the currently loaded embed."
-        execute {
-            val embed = getLoadedEmbed(it.guild!!)!!
-            embed.clearColor()
-            it.respond("Color cleared.")
-        }
-    }
+    fun generateClearCommand(fieldName: String, embedAction: (embed: Embed) -> Unit) =
+        command("Clear$fieldName") {
+            requiresGuild = true
+            description = "Clear the ${fieldName.toLowerCase()} from the currently loaded embed."
+            execute {
+                val embed = getLoadedEmbed(it.guild!!)!!
+                embedAction.invoke(embed)
+                it.respond("$fieldName cleared.")
+            }
+        }!!
 
-    command("ClearDescription") {
-        requiresGuild = true
-        description = "Clear the description from the currently loaded embed."
-        execute {
-            val embed = getLoadedEmbed(it.guild!!)!!
-            embed.clearDescription()
-            it.respond("Description cleared.")
-        }
-    }
-
-    command("ClearFooter") {
-        requiresGuild = true
-        description = "Clear the image from the currently loaded embed."
-        execute {
-            val embed = getLoadedEmbed(it.guild!!)!!
-            embed.clearFooter()
-            it.respond("Footer cleared.")
-        }
-    }
-
-    command("ClearImage") {
-        requiresGuild = true
-        description = "Clear the image from the currently loaded embed."
-        execute {
-            val embed = getLoadedEmbed(it.guild!!)!!
-            embed.clearImage()
-            it.respond("Image cleared.")
-        }
-    }
-
-    command("ClearThumbnail") {
-        requiresGuild = true
-        description = "Clear the thumbnail from the currently loaded embed."
-        execute {
-            val embed = getLoadedEmbed(it.guild!!)!!
-            embed.clearThumbnail()
-            it.respond("Thumbnail cleared.")
-        }
-    }
-
-    command("ClearTimestamp") {
-        requiresGuild = true
-        description = "Clear the timestamp from the currently loaded embed."
-        execute {
-            val embed = getLoadedEmbed(it.guild!!)!!
-            embed.clearTimestamp()
-            it.respond("Timestamp cleared.")
-        }
-    }
-
-    command("ClearTitle") {
-        requiresGuild = true
-        description = "Clear the title of the currently loaded embed."
-        execute {
-            val embed = getLoadedEmbed(it.guild!!)!!
-            embed.clearTitle()
-            it.respond("Title cleared.")
-        }
-    }
+    generateClearCommand("Author") { it.clearAuthor() }
+    generateClearCommand("Color") { it.clearColor() }
+    generateClearCommand("Description") { it.clearDescription() }
+    generateClearCommand("Footer") { it.clearFooter() }
+    generateClearCommand("Image") { it.clearImage() }
+    generateClearCommand("Thumbnail") { it.clearThumbnail() }
+    generateClearCommand("Timestamp") { it.clearTimestamp() }
+    generateClearCommand("Title") { it.clearTitle() }
 }
--- src/main/kotlin/io/github/jakejmattson/embedbot/commands/EditCommands.kt
+++ src/main/kotlin/io/github/jakejmattson/embedbot/commands/EditCommands.kt
@@ -6,52 +6,53 @@ import me.aberrantfox.kjdautils.api.dsl.*
 import me.aberrantfox.kjdautils.internal.command.arguments.*
 import java.time.LocalDateTime
 
-private const val editFormat = "Successfully updated the embed %s!"
+private const val commandResponseFormat = "Successfully updated the embed %s!"
+private const val commandDescriptionFormat = "Set the %s for the currently loaded embed."
 
 @CommandSet("Edit")
 fun editCommands() = commands {
     command("SetAuthor") {
         requiresGuild = true
-        description = "Set the author for the currently loaded embed."
+        description = commandDescriptionFormat.format("author")
         expect(SentenceArg)
         execute {
             val description = it.args.component1() as String
             val embed = getLoadedEmbed(it.guild!!)!!
 
             embed.setAuthor(description)
-            it.respond(editFormat.format("author"))
+            it.respond(commandResponseFormat.format("author"))
         }
     }
 
     command("SetColor") {
         requiresGuild = true
-        description = "Set the color for the currently loaded embed."
+        description = commandDescriptionFormat.format("color")
         expect(HexColorArg)
         execute {
             val color = it.args.component1() as Int
             val embed = getLoadedEmbed(it.guild!!)!!
 
             embed.setColor(color)
-            it.respond(editFormat.format("color"))
+            it.respond(commandResponseFormat.format("color"))
         }
     }
 
     command("SetDescription") {
         requiresGuild = true
-        description = "Set the description for the currently loaded embed."
+        description = commandDescriptionFormat.format("description")
         expect(SentenceArg)
         execute {
             val description = it.args.component1() as String
             val embed = getLoadedEmbed(it.guild!!)!!
 
             embed.setDescription(description)
-            it.respond(editFormat.format("description"))
+            it.respond(commandResponseFormat.format("description"))
         }
     }
 
     command("SetFooter") {
         requiresGuild = true
-        description = "Set the footer for the currently loaded embed."
+        description = commandDescriptionFormat.format("footer")
         expect(UrlArg("Icon URL"), SentenceArg("Text"))
         execute {
             val url = it.args.component1() as String
@@ -59,56 +60,56 @@ fun editCommands() = commands {
             val embed = getLoadedEmbed(it.guild!!)!!
 
             embed.setFooter(text, url)
-            it.respond(editFormat.format("footer"))
+            it.respond(commandResponseFormat.format("footer"))
         }
     }
 
     command("SetImage") {
         requiresGuild = true
-        description = "Set the image for the currently loaded embed."
+        description = commandDescriptionFormat.format("image")
         expect(UrlArg)
         execute {
             val url = it.args.component1() as String
             val embed = getLoadedEmbed(it.guild!!)!!
 
             embed.setImage(url)
-            it.respond(editFormat.format("image"))
+            it.respond(commandResponseFormat.format("image"))
         }
     }
 
     command("SetThumbnail") {
         requiresGuild = true
-        description = "Set the thumbnail for the currently loaded embed."
+        description = commandDescriptionFormat.format("thumbnail")
         expect(UrlArg)
         execute {
             val url = it.args.component1() as String
             val embed = getLoadedEmbed(it.guild!!)!!
 
             embed.setThumbnail(url)
-            it.respond(editFormat.format("thumbnail"))
+            it.respond(commandResponseFormat.format("thumbnail"))
         }
     }
 
     command("SetTimestamp") {
         requiresGuild = true
-        description = "Set the timestamp for the currently loaded embed."
+        description = commandDescriptionFormat.format("timestamp")
         execute {
             val embed = getLoadedEmbed(it.guild!!)!!
             embed.setTimestamp(LocalDateTime.now())
-            it.respond(editFormat.format("timestamp"))
+            it.respond(commandResponseFormat.format("timestamp"))
         }
     }
 
     command("SetTitle") {
         requiresGuild = true
-        description = "Set the title for the currently loaded embed."
+        description = commandDescriptionFormat.format("title")
         expect(SentenceArg)
         execute {
             val title = it.args.component1() as String
             val embed = getLoadedEmbed(it.guild!!)!!
 
             embed.setTitle(title)
-            it.respond(editFormat.format("title"))
+            it.respond(commandResponseFormat.format("title"))
         }
     }
 }
