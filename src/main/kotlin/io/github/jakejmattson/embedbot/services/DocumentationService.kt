5e3dfc58520a4ff96881f18e5fa2a85f2524f888
Jake Mattson <JakeJMattson@gmail.com>
2019-06-30 22:10:02 -0500

    Add templates to DocumentationService

--- src/main/kotlin/io/github/jakejmattson/embedbot/services/DocumentationService.kt
+++ src/main/kotlin/io/github/jakejmattson/embedbot/services/DocumentationService.kt
@@ -11,58 +11,79 @@ class DocumentationService(configuration: Configuration, container: CommandsCont
     init {
         container.commands.getValue("help").category = "Utility"
 
+        val sortOrder =
+            arrayListOf("BotConfiguration", "GuildConfiguration", "Core", "Copy", "Field", "Cluster", "Edit", "Info", "Utility")
+
         if (configuration.generateDocsAtRuntime)
             Timer().schedule(500) {
-                println(generateDocs(container))
+                val commandDocs = generateDocs(container)
+                val sortedDocs = arrayListOf<CategoryDocs>()
+
+                sortOrder.forEach { sortString ->
+                    val category = commandDocs.firstOrNull { sortString == it.name }
+
+                    if (category != null)
+                        sortedDocs.add(category)
+                }
+
+                sortedDocs.forEach {
+                    println(it.toString())
+                }
             }
     }
 
-    private fun generateDocs(commandsContainer: CommandsContainer) =
-        with(StringBuilder()) {
-            data class CommandData(val name: String, val args: String, val description: String) {
-                fun format(format: String) = String.format(format, name, args, description)
-            }
+    data class CategoryDocs(val name: String, val docString: String, private val indentLevel: String = "##") {
+        override fun toString() = "$indentLevel $name\n$docString"
+    }
 
-            operator fun String.times(x: Int) = this.repeat(x)
+    private fun generateDocs(commandsContainer: CommandsContainer): ArrayList<CategoryDocs> {
 
-            commandsContainer.commands.values.groupBy { it.category }.toList().forEach {
-                appendln("## ${it.first}")
+        data class CommandData(val name: String, val args: String, val description: String) {
+            fun format(format: String) = String.format(format, name, args, description)
+        }
 
-                val categoryCommands = it.second.map {
-                    CommandData(
-                        it.name,
-                        it.expectedArgs.joinToString {
-                            if(it.optional)
-                                "(${it.type.name})"
-                            else
-                                it.type.name
-                        }.takeIf { it.isNotEmpty() } ?: "<none>",
-                        it.description.replace("|", "\\|")
-                    )
-                } as ArrayList
+        operator fun String.times(x: Int) = this.repeat(x)
 
-                with(categoryCommands) {
-                    val headers = CommandData("Commands", "Arguments", "Description")
-                    add(headers)
+        val commandDocs = arrayListOf<CategoryDocs>()
 
-                    val longestName = maxBy { it.name.length }!!.name.length
-                    val longestArgs = maxBy { it.args.length }!!.args.length
-                    val longestDescription = maxBy { it.description.length }!!.description.length
-                    val format = "| %-${longestName}s | %-${longestArgs}s | %-${longestDescription}s |"
+        commandsContainer.commands.values.groupBy { it.category }.toList().forEach {
+            val categoryCommands = it.second.map {
+                CommandData(
+                    it.name,
+                    it.expectedArgs.joinToString {
+                        if (it.optional)
+                            "(${it.type.name})"
+                        else
+                            it.type.name
+                    }.takeIf { it.isNotEmpty() } ?: "<none>",
+                    it.description.replace("|", "\\|")
+                )
+            } as ArrayList
 
-                    remove(headers)
+            with(categoryCommands) {
+                val docs = StringBuilder()
 
-                    appendln(headers.format(format))
-                    appendln(String.format(format, "-" * longestName, "-" * longestArgs, "-" * longestDescription))
+                val headers = CommandData("Commands", "Arguments", "Description")
+                add(headers)
 
-                    sortedBy { it.name }.forEach {
-                        appendln(it.format(format))
-                    }
+                val longestName = maxBy { it.name.length }!!.name.length
+                val longestArgs = maxBy { it.args.length }!!.args.length
+                val longestDescription = maxBy { it.description.length }!!.description.length
+                val format = "| %-${longestName}s | %-${longestArgs}s | %-${longestDescription}s |"
+
+                remove(headers)
+
+                docs.appendln(headers.format(format))
+                docs.appendln(String.format(format, "-" * longestName, "-" * longestArgs, "-" * longestDescription))
+
+                sortedBy { it.name }.forEach {
+                    docs.appendln(it.format(format))
                 }
 
-                appendln()
+                commandDocs.add(CategoryDocs(it.first, docs.toString()))
             }
-
-            this.toString()
         }
+
+        return commandDocs
+    }
 }
