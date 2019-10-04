6a6dfb051098caec85627426444feb45c2c4375e
Jake Mattson <JakeJMattson@gmail.com>
2019-10-03 23:33:38 -0500

    Fix mention listener

--- src/main/kotlin/io/github/jakejmattson/embedbot/listeners/MessageListener.kt
+++ src/main/kotlin/io/github/jakejmattson/embedbot/listeners/MessageListener.kt
@@ -2,6 +2,7 @@ package io.github.jakejmattson.embedbot.listeners
 
 import com.google.common.eventbus.Subscribe
 import io.github.jakejmattson.embedbot.services.InfoService
+import me.aberrantfox.kjdautils.extensions.stdlib.trimToID
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 
 class MessageListener(private val infoService: InfoService) {
@@ -10,7 +11,7 @@ class MessageListener(private val infoService: InfoService) {
         with (event) {
             if (author.isBot) return
 
-            if (message.contentRaw == jda.selfUser.asMention)
+            if (message.contentRaw.trimToID() == channel.jda.selfUser.id)
                 channel.sendMessage(infoService.botInfo(guild)).queue()
         }
     }
