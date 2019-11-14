6215ecce01c648706da95510a485b8b4ec2af287
Jake Mattson <JakeJMattson@gmail.com>
2019-11-13 23:46:44 -0600

    Add permission and empty checks

--- src/main/kotlin/io/github/jakejmattson/embedbot/commands/AdvancedCommands.kt
+++ src/main/kotlin/io/github/jakejmattson/embedbot/commands/AdvancedCommands.kt
@@ -1,12 +1,13 @@
 package io.github.jakejmattson.embedbot.commands
 
 import io.github.jakejmattson.embedbot.extensions.*
+import io.github.jakejmattson.embedbot.services.PermissionsService
 import me.aberrantfox.kjdautils.api.dsl.command.*
 import me.aberrantfox.kjdautils.internal.arguments.SentenceArg
 import me.aberrantfox.kjdautils.internal.command.CommandStruct
 
 @CommandSet("Advanced")
-fun advancedCommands() = commands {
+fun advancedCommands(permissionsService: PermissionsService) = commands {
     command("ExecuteAll") {
         description = "Execute a batch of commands in sequence."
         execute(SentenceArg("Commands")) { event ->
@@ -24,10 +25,18 @@ fun advancedCommands() = commands {
                 val command = container[commandName]
                     ?: return@execute event.respond("Unknown command: $commandName")
 
+                val hasPermission = permissionsService.hasClearance(event.message.member!!, command.requiredPermissionLevel)
+
+                if (!hasPermission)
+                    return@mapNotNull null
+
                 val args = split.drop(1)
 
                 command to args
             }
+
+            if (commandMap.isEmpty())
+                return@execute event.respond("No commands to execute!")
 
             commandMap.forEach { (command, args) ->
                 println("Command: ${command.names} - Args: $args")
